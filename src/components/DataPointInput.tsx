8a99fee77d909548ce93c725aadcdec76e1f56f5
Robbe Hovaert <robbe.hovaert@student.odisee.be>
2023-08-10 00:34:05 +0200

    fixed mobile ui

--- src/components/DataPointInput.tsx
+++ src/components/DataPointInput.tsx
@@ -1,8 +1,9 @@
-import {Grid, IconButton, InputAdornment, Paper, TextField} from "@mui/material";
+import {Box, Grid, IconButton, InputAdornment, Paper, TextField} from "@mui/material";
 import {useId} from "react";
 import WaterDropIcon from '@mui/icons-material/WaterDrop';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import CloseIcon from '@mui/icons-material/Close';
+import {useIsMobile, useThemeBreakPoint} from "../hooks/themeBreakPoint.ts";
 
 type DataPointProps = {
     milliliters: string,
@@ -12,14 +13,16 @@ type DataPointProps = {
     onRemove: () => void,
 }
 export default function DataPointInput(props: DataPointProps) {
+    const isMobile = useThemeBreakPoint('sm');
     const id = useId();
+
     return (
         <Paper sx={{
             position: 'relative',
             width: '100%',
             padding: '1rem',
         }}>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} paddingRight={isMobile ? '1.5rem' : undefined}>
                 <Grid item sm={6} xs={12}>
                     <TextField
                         value={props.milliliters}
@@ -54,17 +57,36 @@ export default function DataPointInput(props: DataPointProps) {
                         }}
                     />
                 </Grid>
-                <Grid item sm={1} xs={12} sx={{
-                    display: 'grid',
-                    placeItems: 'center',
-                }}>
-                    <IconButton
-                        onClick={() => props.onRemove()}
-                    >
-                        <CloseIcon/>
-                    </IconButton>
-                </Grid>
+                {
+                    !isMobile && (
+                        <Grid item sm={1} xs={12} sx={{
+                            display: 'grid',
+                            placeItems: 'center',
+                        }}>
+                            <IconButton
+                                onClick={() => props.onRemove()}
+                            >
+                                <CloseIcon/>
+                            </IconButton>
+                        </Grid>
+                    )
+                }
             </Grid>
+            {
+                isMobile && (
+                    <Box sx={{
+                        position: 'absolute',
+                        top: '0',
+                        right: '0'
+                    }}>
+                        <IconButton
+                            onClick={() => props.onRemove()}
+                        >
+                            <CloseIcon/>
+                        </IconButton>
+                    </Box>
+                )
+            }
         </Paper>
     )
 }
