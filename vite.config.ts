cf13d8684f404da4858b9a3df30574a68f2eea1b
Robbe Hovaert <robbe.hovaert@student.odisee.be>
2023-08-11 19:33:45 +0200

    pwa support added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+      VitePWA({
+        injectRegister: null,
+          manifest: {
+            name: 'Nestcafe calculator',
+              short_name: 'NestcafeCalc',
+              description: 'A calculator for Nestcafe',
+              theme_color: '#FFE8D6',
+              icons: [
+                  {
+                      src: 'pwa-64x64.png',
+                      sizes: '64x64',
+                      type: 'image/png'
+                  },
+                  {
+                      src: 'pwa-192x192.png',
+                      sizes: '192x192',
+                      type: 'image/png'
+                  },
+                  {
+                      src: 'pwa-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: 'any'
+                  },
+                  {
+                      src: 'maskable-icon-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: 'maskable'
+                  }
+              ]
+          }
+      })
+  ],
 })
